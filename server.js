1bd63f357359b496c2e500c167f5b1f1ca3b97f6
Davron Yuldashev <yul.davron.93@gmail.com>
2021-06-28 14:50:36 +0500

    pm2

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const next = require('next')
+const dev = process.env.NODE_ENV !== 'production'
+
+console.log(process.env)
+
+const app = next({ dev })
+const router = express.Router()
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+  server.post('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(process.env.PORT, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${process.env.PORT}`)
+  })
+})
