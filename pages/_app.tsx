4aeeb0f8cb2de35828eec9c3b0706dce69553483
Davr <davr@Davrs-NewiMac.local>
2023-03-15 14:43:07 +0500

    dfds

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,21 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 import { ManagedUIContext } from '@components/ui/context'
 import type { AppProps } from 'next/app'
 import { useState } from 'react'
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
   return (
-    <ManagedUIContext>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </ManagedUIContext>
+    <GoogleReCaptchaProvider
+      reCaptchaKey="6LdjO_0kAAAAAEBu0k3SrsLB67XsBASjipXMPY1T"
+      language="RU"
+    >
+      <ManagedUIContext>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </ManagedUIContext>
+    </GoogleReCaptchaProvider>
   )
 }
 export default MyApp
